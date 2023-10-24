8a2c92979753ee55fcb739ccc25aceb057a28a79
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2023-10-24 14:35:46 +0100

    routing and tailwind configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainHomeComponent } from './home/main-home/main-home.component';
+import { SkeletonHomeComponent } from './home/skeleton-home/skeleton-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainHomeComponent,
+    SkeletonHomeComponent
   ],
   imports: [
     BrowserModule,
